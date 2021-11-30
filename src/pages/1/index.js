3043b34899e39c5f1f0b0ecf8920ce8d98454c7e
PabloSzx <pablosaez1995@gmail.com>
2021-11-30 13:48:15 -0300

    update

--- src/pages/1/index.js
+++ src/pages/1/index.js
@@ -3,17 +3,18 @@ import FC from "../../tutor/factorComun/FactorComun";
 import data from "../../tutor/factorComun/ejercicioFC.json";
 import { Stack } from "@chakra-ui/react";
 import { useAction } from "../../utils/action";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
-function IndexPage({ejercicio=data[0]}) {
-  const action=useAction();
+function IndexPage({ ejercicio }) {
+  const action = useAction();
   useEffect(() => {
     action({
       verbName: "loadContent",
-      contentID:"4",
-    //  stepID: ""+ejercicio.steps[0].stepId,
-     // topicID: ""+ejercicio.itemId,
-    })}, [])
+      contentID: "4",
+      //  stepID: ""+ejercicio.steps[0].stepId,
+      // topicID: ""+ejercicio.itemId,
+    });
+  }, []);
 
   return (
     <Stack width="100%" padding="1em">
@@ -21,12 +22,12 @@ function IndexPage({ejercicio=data[0]}) {
     </Stack>
   );
 }
-/*export async function getServerSideProps() {
+export async function getServerSideProps() {
   //const fs = require('fs');
   //const exercise = data[0]
   return {
-    props: {ejercicio:data[2]}, // will be passed to the page component as props
-  }
-}*/
+    props: { ejercicio: data[2] }, // will be passed to the page component as props
+  };
+}
 
 export default IndexPage;
--- src/pages/2/index.js
+++ src/pages/2/index.js
@@ -2,29 +2,30 @@ import FCC from "../../tutor/factorComunCompuesto/FactorComunCompuesto";
 import data from "../../tutor/factorComunCompuesto/ejerciciosFCC.json";
 import { Stack } from "@chakra-ui/react";
 import { useAction } from "../../utils/action";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
-function IndexPage({ejercicio=data[0]}) {
-  const action=useAction();
+function IndexPage({ ejercicio }) {
+  const action = useAction();
   useEffect(() => {
     action({
       verbName: "loadContent",
-      contentID:"5",
-    //  stepID: ""+ejercicio.steps[0].stepId,
-     // topicID: ""+ejercicio.itemId,
-    })}, [])
+      contentID: "5",
+      //  stepID: ""+ejercicio.steps[0].stepId,
+      // topicID: ""+ejercicio.itemId,
+    });
+  }, []);
   return (
     <Stack width="100%" padding="1em">
       <FCC ejercicio={ejercicio}></FCC>
     </Stack>
   );
 }
-/*export async function getServerSideProps() {
+export async function getServerSideProps() {
   //const fs = require('fs');
   //const exercise = data[0]
   return {
-    props: {ejercicio:data[0]}, // will be passed to the page component as props
-  }
-}*/
+    props: { ejercicio: data[0] }, // will be passed to the page component as props
+  };
+}
 
 export default IndexPage;
